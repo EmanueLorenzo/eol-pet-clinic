99cff46cc93b48d259f6e9200855d89ebef921c7
Emanuel <lorenzoemanueloscar@gmail.com>
2021-11-10 19:47:35 -0300

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package emanuel.springframework.eolpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package emanuel.springframework.eolpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
