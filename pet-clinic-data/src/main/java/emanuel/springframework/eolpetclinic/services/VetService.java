c005999bc1d20986ee0bd1c14f0af255cb8f4399
Emanuel <lorenzoemanueloscar@gmail.com>
2021-11-11 18:39:44 -0300

    refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package emanuel.springframework.eolpetclinic.services;
 
 import emanuel.springframework.eolpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 
 }
