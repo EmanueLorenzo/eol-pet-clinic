298ab84ee868b9edd1d9abb03817f52fa23831c1
Emanuel <lorenzoemanueloscar@gmail.com>
2021-11-11 21:27:27 -0300

    Impl Map based Services, closes #5

---
+++ pet-clinic-data/src/main/java/emanuel/springframework/eolpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package emanuel.springframework.eolpetclinic.services.map;
+
+import emanuel.springframework.eolpetclinic.model.Vet;
+import emanuel.springframework.eolpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findbyID(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id );
+    }
+}
